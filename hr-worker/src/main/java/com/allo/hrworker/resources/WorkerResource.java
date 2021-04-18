f202f02b61bd4d65e9fdafaa7e865ceebac85829
Alberto Lopes <alberto_llopes@hotmail.com>
2021-04-18 15:35:45 -0300

    Spring eureka configuration

--- hr-worker/src/main/java/com/allo/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/allo/hrworker/resources/WorkerResource.java
@@ -4,7 +4,10 @@ import com.allo.hrworker.entities.Worker;
 import com.allo.hrworker.repositories.WorkRepository;
 import lombok.AllArgsConstructor;
 import org.hibernate.jdbc.Work;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +21,10 @@ import java.util.List;
 @AllArgsConstructor
 public class WorkerResource {
 
+    private static Logger log = LoggerFactory.getLogger(WorkerResource.class);
+
+    private final Environment env;
+
     private final WorkRepository repository;
 
     @GetMapping
@@ -28,6 +35,8 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        log.info("Port = " + env.getProperty("local.server.port"));
+
         Worker worker = repository.findById(id).orElseThrow();
         return ResponseEntity.ok(worker);
     }
